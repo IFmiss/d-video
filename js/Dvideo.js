276c6cf34cc92535e4e82b40dc85a47074b61faa
ifmiss <185098535@qq.com>
2017-10-25 16:16:32 +0800

    添加 音量控制 展示效果优化

--- js/Dvideo.js
+++ js/Dvideo.js
@@ -19,24 +19,57 @@
 		// 判断是否是new Dvideo 的  不是的话 帮他new一下
 		if (!(this instanceof Dvideo)) return new Dvideo(options)
 		this.localValue = {
+			// video元素
 			ele: '',
+
+			// 地址
 			src: 'http://www.daiwei.org/index/video/EnV%20-%20PneumaticTokyo.mp4',
+			
+			// 显示的名称
 			title: '这是一个视频标题这是一个视频标题这是一个视频标题这是一个视频标题',
+			
+			// 宽度
 			width: '420px',
+			// 高度
 			height: '250px',
+
+			// 是否显示播放下一集的提示按钮
 			showNext: false,
+
+			// 是否设置自动播放
 			autoplay: false,
+
+			// 控制栏显示隐藏动画的 时间
 			ctrSpeedDuration: 5000,
+			
+			// 是否自动循环
 			loop: true,
+
+			// 是否显示音量的控制效果
+			showVolume: true,
+			// 在非全屏下是否显示控制
+			showVolumeUnFull: false,
+
+			// 是否显示语速的设置
+			showPlayBackRate: true,
 			playbackRate: {
+				// 索引
 				activeIndex: 1,
+				// 选项
 				rateList: [0.8,1,1.4,1.8,2]
 			},
+
+			// 是否显示清晰度的设置
+			showVideoDefinition: true,
 			videoDefinition: {
+				// 索引
 				activeIndex: 1,
+				// 选项
 				definitionList: [
 					{
+						// 类型  字符串标识
 						type: '0',
+						// 类型  展示效果标识
 						name: '标清'
 					},
 					{
@@ -49,8 +82,9 @@
 					}
 				]
 			},
-			// 可让用户自定义扩展
+			// 可让用户自定义扩展   播放下一个视频的操作
 			nextVideoExtend: function () {alert(1)},
+			// 设置清晰度的操作
 			setVideoDefinition: function () {}
 		}
 
@@ -77,8 +111,13 @@
 		this.maxProgressWidth = 0
 		// 进度条拖动的位置
 		this.dragProgressTo = 0
+		
 		// 音量大小
 		this.volume = 1
+		// 音量最大宽度
+		this.maxVolumeWidth = 0
+		// 音量拖动的位置
+		this.dragVolumeTo = 0
 
 		// 获取浏览器版本
 		this.browserV = this.browserVersion()
@@ -100,7 +139,8 @@
 	Dvideo.prototype = {
 		constructor: this,
 		initDom: function () {
-			this.opt.ele.style.cssText = 'width:' + this.opt.width + '; height: ' + this.opt.height
+			this.opt.ele.style.width = this.opt.width
+			this.opt.ele.style.height = this.opt.height
 
 			// 创建组件的content区域
 			this.createVideoC()
@@ -128,12 +168,21 @@
 			this.menuRightC.className = 'Dvideo-menu-right-content'
 			this.videoCtrlDetail.appendChild(this.menuRightC)
 
+			// 音量拖动效果
+			if (this.opt.showVolume) {
+				this.createVolume()
+			}
+
 			// 语速选项的列表
-			this.createPlaybackRateList()
+			if (this.opt.showPlayBackRate) {
+				this.createPlaybackRateList()
+			}
 
 
 			// 设置清晰度区域
-			this.createVideoDefinition()
+			if (this.opt.showVideoDefinition) {
+				this.createVideoDefinition()
+			}
 
 
 			// 全屏按钮
@@ -141,7 +190,6 @@
 
 			// 显示提示信息
 			this.createVideoTips()
-
 
 			// 初始化事件
 			this.initEvent()
@@ -180,9 +228,8 @@
 
 		// 全屏下视频的样式
 		launchFullScreenStyle: function () {
-			// element.style.cssText = 'width: 100%; height: 100%;'
-			this.opt.ele.style.cssText = 'width: 100%; height: 100%;'
-			// this.videoEle.style.cssText = 'width: 100%; height: 100%;'
+			this.opt.ele.style.width = '100%'
+			this.opt.ele.style.height = '100%'
 		},
 
 		// 全屏下IE 11 以下视频的样式
@@ -232,14 +279,16 @@
 		exitFullScreenStyle: function () {
 			console.log('关闭全屏 其他浏览器 和 非IE11以下')
 			this.updateFullScreenState(false)
-			this.opt.ele.style.cssText = 'width:' + this.opt.width + '; height: ' + this.opt.height
-			// this.videoEle.style.cssText = 'width:' + this.opt.width + '; height: ' + this.opt.height
+			this.opt.ele.style.width = this.opt.width
+			this.opt.ele.style.height = this.opt.height
 		},
 
 		// 更新全屏状态  包括显示全屏图标样式    
 		updateFullScreenState: function (bool) {
 			this.isFull = bool || false
+			// 全屏图标样式
 			var iconClassName = this.isFull ? 'Dvideo-menu-fullscreenConfig icon-canclefullscreen' : 'Dvideo-menu-fullscreenConfig icon-fullscreen'
+			// 文案
 			var title = this.isFull ? '取消全屏' : '全屏'
 			this.fullscreenConfig.className = iconClassName
 			this.fullscreenConfig.title = title
@@ -381,14 +430,15 @@
 			}
 		},
 
-		showLoading: function (bool) {
+		showLoading: function (bool, text) {
+			var text = text || '视频加载中,请稍等  或者切换稍低的清晰度'
 			if (bool) {
-				this.tipsInfo.innerText = '视频加载中,请稍等  或者切换稍低的清晰度'
+				this.tipsInfo.innerText = text
 				this.tipsInfo.style.display = 'block'
 				console.log('加载中')
 			} else {
 				this.tipsInfo.style.display = 'none'
-				// console.log('正常播放')
+				console.log('正常播放')
 			}
 		},
 
@@ -453,15 +503,17 @@
 		// 	_this.initVideoEvent()
 		// },
 		setVideoDefinition: function (e) {
+			// 显示提示信息
+			this.showLoading(true, '视频清晰度切换中，请稍等')
+
+			// 获取索引 以及清晰度类型
 			var index = e.target.getAttribute('data-index')
 			var type = e.target.getAttribute('data-type')
 
 			this.currentT = this.videoEle.currentTime
-
-			this.opt.setVideoDefinition(type, e)
 			
-			this.videoEle.currentTime = this.currentT
-			this.videoPlay()
+			// this.videoEle.currentTime = this.currentT
+			// this.videoPlay()
 
 			this.opt.videoDefinition = {
 				activeIndex: index,
@@ -478,10 +530,12 @@
 			this.getDomByClass('Dvideo-definition-list active')[0].className = 'Dvideo-definition-list'
 			e.target.className = 'Dvideo-definition-list active'
 			this.videoDefinitionC.style.display = 'none'
+
+
+			this.opt.setVideoDefinition(type, e, this.currentT)
 		},
 
-		setPlayBackRate: function (e) {
-			var index = e.target.getAttribute('data-index')
+		setPlayBackRate: function (index) {
 			this.playbackR = this.opt.playbackRate.rateList[index]
 			this.videoEle.playbackRate = this.playbackR
 			this.playbackRateText.title = this.playbackR.toFixed(1) + ' x'
@@ -497,7 +551,7 @@
 
 			// 设置列表active状态
 			this.getDomByClass('Dvideo-playbackRate-list active')[0].className = 'Dvideo-playbackRate-list'
-			e.target.className = 'Dvideo-playbackRate-list active'
+			this.getDomByClass('Dvideo-playbackRate-list')[index].className = 'Dvideo-playbackRate-list active'
 			this.playbackRateC.style.display = 'none'
 		},
 
@@ -587,7 +641,8 @@
 
 			_this.playbackRateC.onclick = function (event) {
 				var e = event || window.event
-				_this.setPlayBackRate(e)
+				var index = e.target.getAttribute('data-index')
+				_this.setPlayBackRate(index)
 			}
 		},
 
@@ -709,7 +764,6 @@
 			this.videoEle.src = this.opt.src
 			this.videoEle.loop = this.opt.loop
 			this.videoEle.autoplay = this.opt.autoplay
-			// this.videoEle.style.cssText = 'width: 100%; height: auto'
 			this.videoC.appendChild(this.videoEle)
 
 
@@ -722,6 +776,7 @@
 				_this.videoPlayPauseI.title = '暂停 ctrl + space'
 				var date = new Date ()
 				_this.reduceTBefore = Date.parse(date) - Math.floor(_this.videoEle.currentTime * 1000)
+				_this.showLoading(false)
 			},
 			_this.videoEle.onpause = function () {
 				_this.isPlaying = false
@@ -743,7 +798,6 @@
 					_this.updatePorgress()
 					var date = new Date ()
 					_this.reduceTBefore = Date.parse(date) - Math.floor(_this.currentT * 1000)
-					_this.showLoading(false)
 				}
 			},
 			_this.videoEle.onprogress = function () {
@@ -781,7 +835,6 @@
 			// video content
 			this.videoC = document.createElement('div')
 			this.videoC.className = 'Dvideo-content'
-			// this.videoC.style.cssText = 'width:' + this.opt.width + '; height: ' + this.opt.height
 			this.opt.ele.appendChild(this.videoC)
 
 			var _this = this
@@ -972,6 +1025,105 @@
 			_this.videoDefinition.onclick = function (event) {
 				var e = event || window.event
 				_this.setVideoDefinition(e)
+			}
+		},
+
+		// 创建设置音量的效果
+		createVolume: function () {
+			var showVClassName = this.opt.showVolumeUnFull ? '' : 'none'
+			// 设置清晰度区域
+			this.videoVolumeC = document.createElement('div')
+			this.videoVolumeC.className = 'Dvideo-volume ' + showVClassName
+			this.menuRightC.appendChild(this.videoVolumeC)
+
+			// 进度线条
+			this.videoVolumeP = document.createElement('div')
+			this.videoVolumeP.className = 'Dvideo-volume-P'
+			this.videoVolumeC.appendChild(this.videoVolumeP)
+
+			// 事实的进度信息
+			this.videoVolumeR = document.createElement('div')
+			this.videoVolumeR.className = 'Dvideo-volume-R'
+			this.videoVolumeP.appendChild(this.videoVolumeR)
+
+			// 可拖动的小圆点
+			this.videoVolumeRange = document.createElement('div')
+			this.videoVolumeRange.className = 'Dvideo-volume-range'
+			this.videoVolumeC.appendChild(this.videoVolumeRange)
+
+			// 初始化音量 有本地存储 设置过就使用本地存储
+			this.initVolume ();
+
+			var isDrag = false
+
+			var _this = this
+			var persent = 0
+			// 进度条拖动 (PC)
+			_this.videoVolumeRange.onmousedown = function (event) {
+				isDrag = true
+				var e = event || window.event
+				var x = e.clientX
+				var l = event.target.offsetLeft + 6
+				e.stopPropagation()
+				_this.maxVolumeWidth = _this.videoVolumeC.offsetWidth
+
+				_this.videoVolumeC.onmousemove = function (event) {
+					if(isDrag) {
+						var e = event || window.event
+						var thisX = e.clientX
+						_this.dragVolumeTo = Math.min(_this.maxVolumeWidth, Math.max(0, l + (thisX - x)))
+						persent = _this.dragVolumeTo / _this.maxVolumeWidth
+						console.log(persent)
+						_this.updateVolume(persent);
+					}
+				}
+
+				_this.videoVolumeC.onmouseup = function (event) {
+					isDrag = false
+					e.stopPropagation()
+					e.preventDefault()
+					// 本地存储
+					_this.setLStorage('Dvideo-volume', persent)
+					return
+				}
+
+				_this.videoVolumeC.onmouseleave = function (event) {
+					isDrag = false
+					e.stopPropagation()
+					e.preventDefault()
+					// 本地存储
+					_this.setLStorage('Dvideo-volume', persent)
+					return
+				}
+			}
+
+			_this.videoVolumeP.onclick = function (event) {
+				var e = event || window.event
+				// alert(e.target.offsetLeft)
+				var videoVolumeCW = _this.videoVolumeP.offsetWidth
+				var eW = e.offsetX
+				// console.log(videoVolumeCW)
+				_this.updateVolume(eW / videoVolumeCW);
+				// 本地存储
+				_this.setLStorage('Dvideo-volume', eW / videoVolumeCW)
+			}
+		},
+
+		// 更新音量界面显示以及音量调整
+		updateVolume: function (persent) {
+			this.videoVolumeRange.style.left = persent * 100 + '%'
+			this.videoVolumeR.style.width = persent * 100 + '%'
+			this.videoVolumeRange.setAttribute('data-volume', Math.round(persent * 100))
+			this.videoEle.volume = persent
+		},
+
+		// 初始化音量
+		initVolume: function () {
+			if(this.getLStorage('Dvideo-volume') === null) {
+				this.updateVolume(0.8)
+			} else {
+				var persent = this.getLStorage('Dvideo-volume')
+				this.updateVolume(persent)
 			}
 		},
 
