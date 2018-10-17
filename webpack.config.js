ff43c741b3cf3fd7d6d496c99e2a899439124504
DavidDai <185098535@qq.com>
2018-10-17 14:21:51 +0800

    修改package.json依赖的版本

--- webpack.config.js
+++ webpack.config.js
@@ -17,23 +17,24 @@ const resolve = function (dir) {
 
 module.exports = {
 	// 正常run的 配置
-	// entry: './src/index.js',
-	// path: path.resolve(__dirname, 'dist'),
-	// 	publicPath: '',
-	// 	filename: '[name]-[hash].js'
-	// },
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '',
+		filename: '[name]-[hash].js'
+	},
 
 	// 打包发布npm的配置
-	entry: './src/script/Dvideo.js',
-	output: {
-	path: path.resolve(__dirname, 'lib'),
-		publicPath: '',
-		filename: 'd-video.js',
-		// filename: '[name]-[hash].js',
-		library: 'd-video', // library指定的就是你使用require时的模块名，这里便是require("VueMessage")
-		libraryTarget: 'umd', //libraryTarget会生成不同umd的代码，例如可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的。
-		umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define。
-	},
+	// entry: './src/script/Dvideo.js',
+	// output: {
+	// path: path.resolve(__dirname, 'lib'),
+	// 	publicPath: '',
+	// 	filename: 'd-video.js',
+	// 	// filename: '[name]-[hash].js',
+	// 	library: 'd-video', // library指定的就是你使用require时的模块名，这里便是require("VueMessage")
+	// 	libraryTarget: 'umd', //libraryTarget会生成不同umd的代码，例如可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的。
+	// 	umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define。
+	// },
 	module: {
 		rules: [
 			{
